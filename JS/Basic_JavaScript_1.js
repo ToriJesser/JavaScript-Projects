d66e8852fdef20a3d0b5c1dd8c4123297be2fee4
Tori Jesser <tori.jesser@gmail.com>
2019-12-09 12:03:55 -0800

    Javascript assignments

---
+++ JS/Basic_JavaScript_1.js
@@ -0,0 +1,30 @@
+var x = "Save the turtles";
+var y = "How can we save the turtles?"
+var a = "I am" + "groot";
+var Family = "Jesser", Dad = "Josh", Mom = "Tori", Daughter_1 = "Misty", Daughter_2 = "Winry";
+var Family = Family.fontcolor("red");
+var Dad = Dad.fontcolor("blue");
+var Mom = Mom.fontcolor("purple");
+var Daughter_1 = Daughter_1.fontcolor("orange");
+var Daughter_2 = Daughter_2.fontcolor("grey");
+
+
+window.alert("I like turtles");
+
+document.write("Turtles are great");
+
+document.write(x);
+
+window.alert(y);
+
+document.write("Tori told everyone, \"We need to do something to save the turtles!\"<br>\"Ugh that's so hard.\"responded her now ex friends");
+
+document.write("\"I am groot. I am groot,"
++"I am groot, I am groot.\""
++"-Groot");
+
+document.write(a);
+
+document.write(Daughter_1);
+document.write(1+2);
+
