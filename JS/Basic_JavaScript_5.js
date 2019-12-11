a9d9db6b0d20dca279d7232a18865276424fec93
Tori Jesser <tori.jesser@gmail.com>
2019-12-11 09:55:04 -0800

    New assignment
    Assignment 5

---
+++ JS/Basic_JavaScript_5.js
@@ -0,0 +1,59 @@
+document.writeln(typeof 7);
+
+function myFunction() {
+    document.getElementById("Test").innerHTML = isNaN('This is a string')
+}
+
+function myFunction_2() {
+    document.getElementById("Test_1").innerHTML = isNaN('777')
+}
+
+document.writeln(69E420);
+
+document.writeln(-69E420);
+
+document.writeln(69<420);
+
+document.writeln(69>420);
+
+console.log(2*2);
+
+document.writeln("69" + 420);
+
+document.writeln(console.log(4*2)<2);
+
+document.writeln(10==10);
+
+document.writeln(10==2);
+
+X = 69;
+Y = 69;
+document.writeln(X===Y);
+
+Z = 69;
+T = 420;
+document.writeln(Z===T);
+
+F = "420";
+G = 420;
+document.writeln(F===G);
+
+H = 420;
+I = "Hello";
+document.writeln(H===I);
+
+document.writeln(10 > 2 && 6 > 3);
+
+document.writeln(10 < 4 && 13 > 2);
+
+document.writeln(5 > 1 || 5 < 1);
+
+document.writeln(5 < 2 || 10 < 8);
+
+function notfunction() {
+    document.getElementById("not").innerHTML = !(5 > 10);
+}
+
+function not_function () {
+    document.getElementById("not_1").innerHTML = !(5 < 10);
+}
